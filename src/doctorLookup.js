a9d27826ba80b205c6f61ec2e83032798f07c12e
Chan Lee <chanethanlee@gmail.com>
2018-10-19 10:18:27 -0700

    add very basic function to look up with name and symtom.

---
+++ src/doctorLookup.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+var Promise = require('es6-promise').Promise;
+
+export class DoctorLookup{
+  findDocBySymptom(symptom, name, page){
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=or-portland&skip=${page}&limit=10&user_key=${process.env.exports.apiKey}`;
+
+
+      request.onload = function(){
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
