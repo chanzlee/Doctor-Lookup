c28ebc9a30d7e98eb21cecb997cd56ccbf9720e2
Chan Lee <chanethanlee@gmail.com>
2018-10-19 11:29:48 -0700

    edit main function to work as factory function.

--- src/doctorLookup.js
+++ src/doctorLookup.js
@@ -2,7 +2,7 @@
 var Promise = require('es6-promise').Promise;
 
 export class DoctorLookup{
-  findDocBySymptom(symptom, name, page){
+  findDoctor(symptom, name, location, page){
     return new Promise(function(resolve, reject){
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=or-portland&skip=${page}&limit=10&user_key=${process.env.exports.apiKey}`;
@@ -18,5 +18,17 @@ export class DoctorLookup{
       request.open("GET", url, true);
       request.send();
     });
+  }
+
+  findDoctorBySymptom(symptom,page) {
+    return this.findDoctor(symptom,"","",page);
+  }
+
+  findDoctorByName(name,page) {
+    return this.findDoctor("",name,"",page);
+  }
+
+  findDoctorByLocation(location,page) {
+    return this.findDoctor("","",location,page);
   }
 }
