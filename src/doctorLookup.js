eaa581d5fcf43993ab730e7ef750a15b94603519
Chan Lee <chanethanlee@gmail.com>
2018-10-19 13:25:05 -0700

    link with geocode.js to return location based promise.

--- src/doctorLookup.js
+++ src/doctorLookup.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 var Promise = require('es6-promise').Promise;
+import {Geocode} from './geocode.js'
 
 export class DoctorLookup{
   findDoctor(symptom, name, location, page){
+    location = location || "or-portland";
     return new Promise(function(resolve, reject){
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=or-portland&skip=${page}&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${symptom}&location=${location}&skip=${page}&limit=10&user_key=${process.env.exports.apiKey}`;
 
 
       request.onload = function(){
@@ -29,6 +31,21 @@ export class DoctorLookup{
   }
 
   findDoctorByLocation(location,page) {
-    return this.findDoctor("","",location,page);
+    let self = this;
+    console.log("doc-loc");
+    let newGeocode = new Geocode();
+    let geoPromise = newGeocode.findLngLat(location);
+    let locationKey = "";
+    return geoPromise.then(function(response){
+      console.log("geo-loc");
+      let geoBody = JSON.parse(response);
+      let lng = geoBody.postalCodes[0].lng.toString();
+      let lat = geoBody.postalCodes[0].lat.toString();
+      locationKey = lat+","+lng+","+10;
+      console.log(locationKey);
+
+      let docPromise = self.findDoctor("","",locationKey,page);
+      return docPromise;
+    });
   }
 }
